a5868971f88028a8d897ab9020f89bf35a0510b4
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-12 05:33:17 -0400

    update; Oninit and perfect scrollbar

--- exordium-backend/src/app/app.component.ts
+++ exordium-backend/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import PerfectScrollbar from 'perfect-scrollbar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'exordium-backend';
+
+  ngOnInit(): void {
+    const container = document.querySelector('.c-sidebar-nav');
+    const ps = new PerfectScrollbar(container);
+  }
 }
