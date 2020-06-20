45a69e1df319010ef7647a52f48907af84561dac
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-20 05:26:42 -0400

    update; directive for showing and unshowing navbar submenus

--- exordium-backend/src/app/__directives/navbar.directive.ts
+++ exordium-backend/src/app/__directives/navbar.directive.ts
@@ -11,15 +11,17 @@ export class NavbarDirective {
         private renderer: Renderer2
     ) { }
 
-    @HostListener('click', ['$event']) onClick(event) {
-        if (this.elementRef.nativeElement.classList.contains('c-show')) {
-            this.renderer.removeClass(this.elementRef.nativeElement, 'c-show');
+    @HostListener('click', ['$event']) onClick(event: any) {
+        if (this.elementRef.nativeElement.classList.contains('show')) {
+            this.renderer.removeClass(this.elementRef.nativeElement, 'show');
             this.renderer.setAttribute(this.elementRef.nativeElement.childNodes[0], 'aria-expanded', 'false')
-            this.renderer.removeClass(this.elementRef.nativeElement.childNodes[1], 'c-show')
+            this.renderer.removeClass(this.elementRef.nativeElement.childNodes[1], 'show')
         } else {
-            this.renderer.addClass(this.elementRef.nativeElement, 'c-show')
+            this.renderer.addClass(this.elementRef.nativeElement, 'show')
             this.renderer.setAttribute(this.elementRef.nativeElement.childNodes[0], 'aria-expanded', 'true')
-            this.renderer.addClass(this.elementRef.nativeElement.childNodes[1], 'c-show')
+            this.renderer.addClass(this.elementRef.nativeElement.childNodes[1], 'show')
         }
+
+        event.preventDefault();
     }
 }
