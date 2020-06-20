768617d4d65877deada559fdca4bc0481fda6e8e
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-20 05:27:19 -0400

    add; network status client page

---
+++ exordium-backend/src/app/_pages/dashboard/clients/network-status/network-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-network-status',
+  templateUrl: './network-status.component.html',
+  styleUrls: ['./network-status.component.scss']
+})
+export class NetworkStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
