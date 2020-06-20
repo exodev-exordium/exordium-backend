9b7d4224897317aaaa508349ff3999eeee92be85
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-20 05:30:19 -0400

    update; remove perfect scrollbar (moved to the dashboard app componenet

--- exordium-backend/src/app/app.component.ts
+++ exordium-backend/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import PerfectScrollbar from 'perfect-scrollbar';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,7 +9,6 @@ export class AppComponent implements OnInit {
   title = 'exordium-backend';
 
   ngOnInit(): void {
-    const container = document.querySelector('.c-sidebar-nav');
-    const ps = new PerfectScrollbar(container);
+  
   }
 }
