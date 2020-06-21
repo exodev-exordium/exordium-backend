54c52b52ba05865288e40b9ebcb881a6c8979318
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-21 04:49:26 -0400

    update; enable routing tracing

--- exordium-backend/src/app/app-routing.module.ts
+++ exordium-backend/src/app/app-routing.module.ts
@@ -13,7 +13,14 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {
+        enableTracing: true
+      }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
