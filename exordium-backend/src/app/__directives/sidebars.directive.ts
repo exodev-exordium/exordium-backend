c5b2a01162325c1b435e5370e9eb2d5076fffb78
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-22 07:42:10 -0400

    update app-dashboard; remove old sidebar jquery code and created a directive instead.

--- exordium-backend/src/app/__directives/sidebars.directive.ts
+++ exordium-backend/src/app/__directives/sidebars.directive.ts
@@ -1,16 +1,69 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
 import $ from 'jquery';
 
 @Directive({
-  selector: '[appSidebars]'
+  selector: '[sidebarhandler]'
 })
-export class SidebarsDirective {
+export class SidebarsDirective implements OnInit {
 
   constructor(
     private elementRef: ElementRef
   ) { }
 
+  ngOnInit(): void {
+    $(document).on("click", (event) => {
+      const sidebar = $('#sidebar');
+      const aside = $('#aside');
+      const backdrop = $('.sidebar-backdrop');
+
+      if (sidebar.hasClass('sidebar-show')) {
+        if (!$(event.target).closest("#sidebar").length && !$(event.target).closest(".sidebar-toggle").length) {
+          sidebar.removeClass('sidebar-show');
+          backdrop.removeClass('show');
+          $("body").css('overflow', '');
+        }
+      }
+
+      if (aside.hasClass('sidebar-show')) {
+        if (!$(event.target).closest("#aside").length && !$(event.target).closest(".aside-toggle").length) {
+          aside.removeClass('sidebar-show');
+          backdrop.removeClass('show');
+          $("body").css('overflow', '');
+        }
+      }
+    });
+  }
+
   @HostListener('click', ['$event']) onClick(event: any) {
+    const backdrop = $('.sidebar-backdrop');
+
+    if (this.elementRef.nativeElement.classList.contains('sidebar-toggle')) {
+      const sidebar = $('#sidebar');
+
+      if (sidebar.hasClass('sidebar-show')) {
+        sidebar.removeClass('sidebar-show');
+        backdrop.removeClass('show');
+        $("body").css('overflow', '');
+      } else {
+        sidebar.addClass('sidebar-show');
+        backdrop.addClass('show');
+        $("body").css('overflow', 'hidden');
+      }
+    } 
+    
+    if (this.elementRef.nativeElement.classList.contains('aside-toggle')) {
+      const aside = $('#aside');
+
+      if (aside.hasClass('sidebar-show')) {
+        aside.removeClass('sidebar-show');
+        backdrop.removeClass('show');
+        $("body").css('overflow', '');
+      } else {
+        aside.addClass('sidebar-show');
+        backdrop.addClass('show');
+        $("body").css('overflow', 'hidden');
+      }
+    }
 
     event.preventDefault();
   }
--- exordium-backend/src/app/_pages/dashboard/app-dashboard.component.ts
+++ exordium-backend/src/app/_pages/dashboard/app-dashboard.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import $ from 'jquery';
-
 @Component({
   selector: 'app-app-dashboard',
   templateUrl: './app-dashboard.component.html',
@@ -12,61 +10,7 @@ export class AppDashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // toggle the sidebar and aside
-    this.toggleSides();
-  }
 
-  toggleSides() {
-    const sidebar = $('#sidebar');
-    const aside = $('#aside');
-    const backdrop = $('.sidebar-backdrop');
-
-    const toggleButtonSidebar = $('.sidebar-toggle');
-    const toggleButtonAside = $('.aside-toggle');
-
-    // sidebar toggle
-    toggleButtonSidebar.click(() => {
-      if (sidebar.hasClass('sidebar-show')) {
-        sidebar.removeClass('sidebar-show');
-        backdrop.removeClass('show');
-        $("body").css('overflow', '');
-      } else {
-        sidebar.addClass('sidebar-show');
-        backdrop.addClass('show');
-        $("body").css('overflow', 'hidden');
-      }
-    });
-
-    // aside toggle
-    toggleButtonAside.click(() => {
-      if (aside.hasClass('sidebar-show')) {
-        aside.removeClass('sidebar-show');
-        backdrop.removeClass('show');
-        $("body").css('overflow', '');
-      } else {
-        aside.addClass('sidebar-show');
-        backdrop.addClass('show');
-        $("body").css('overflow', 'hidden');
-      }
-    });
-
-    $(document).on("click", (event) => {
-      if (sidebar.hasClass('sidebar-show')) {
-        if (!$(event.target).closest("#sidebar").length && !$(event.target).closest(".sidebar-toggle").length) {
-          sidebar.removeClass('sidebar-show');
-          backdrop.removeClass('show');
-          $("body").css('overflow', '');
-        }
-      }
-
-      if (aside.hasClass('sidebar-show')) {
-        if (!$(event.target).closest("#aside").length && !$(event.target).closest(".aside-toggle").length) {
-          aside.removeClass('sidebar-show');
-          backdrop.removeClass('show');
-          $("body").css('overflow', '');
-        }
-      }
-    });
   }
 
 }
