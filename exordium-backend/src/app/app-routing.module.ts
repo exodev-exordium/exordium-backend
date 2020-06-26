04b9f5a846d395227822e472fa17dfcb2df71ba8
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-26 00:35:25 -0400

    update; app routing should go to signin, not dashboard

--- exordium-backend/src/app/app-routing.module.ts
+++ exordium-backend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/dashboard',
+    redirectTo: 'auth/signin',
     pathMatch: 'full',
     data: {
       title: 'Exordium'
