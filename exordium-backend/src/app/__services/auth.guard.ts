ed27f3c59b4eb5c8ab26b19c7bbdd8680b67677f
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-26 07:13:52 -0400

    initial; auth guard

---
+++ exordium-backend/src/app/__services/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    if (this.authService.isSignedIn !== true) {
+      window.alert('Access is not allowed!');
+      this.router.navigate(['auth/signin']);
+    }
+    
+    return true;
+  }
+  
+}
