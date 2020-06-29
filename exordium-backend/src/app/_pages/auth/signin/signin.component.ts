3fdb0b37afa16840ce132afc05984d61805be526
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-29 11:22:15 -0400

    signin page design

--- exordium-backend/src/app/_pages/auth/signin/signin.component.ts
+++ exordium-backend/src/app/_pages/auth/signin/signin.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { jarallax } from 'jarallax';
+
+import * as $ from 'jquery';
+
+import { AuthService } from 'src/app/__services/auth.service';
+import { UserService } from 'src/app/__services/user.service';
+
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +16,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  signinForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {
+    this.signinForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+      remember: [null],
+      recaptcha: [null, Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.6
+    });
+  }
+
+  get f() {
+    return this.signinForm.controls;
+  }
+
+  onSubmit(): void {
+    const srcButton = $('submitSignin');
+    srcButton.attr('disabled', true);
+    srcButton.addClass('m-progress');
+
+    if (this.signinForm.invalid) {
+      this.toastr.error('Please make sure you fill out all the fields correctly, then try signing in again...');
+    } else {
+      this.authService.signin(this.signinForm.value).subscribe((res) => {
+        this.toastr.success('You have successfully logged in!');
+
+        localStorage.setItem('access_token', res.token);
+
+        this.userService.getUserDataBasic().subscribe((res) => {
+          this.router.navigate([`/dashboard`]);
+        });
+      }, (err) => {
+        this.toastr.success(err[0].msg);
+      });
+
+    }
+
+    setTimeout(() => {
+      srcButton.removeAttr('disabled');
+      srcButton.removeClass('m-progress');
+    }, 2000);
+
   }
 
 }
