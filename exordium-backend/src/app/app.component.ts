5d763b76583e9aebc1aba63e6fea740cc3ec7089
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-29 11:24:36 -0400

    update; title and router animations

--- exordium-backend/src/app/app.component.ts
+++ exordium-backend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Event, Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'exordium-backend';
+
+  constructor (
+    private router: Router,
+    private titleService: Title
+  ) { }
 
   ngOnInit(): void {
-  
+    this.router.events.subscribe((event: Event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {}
+        case event instanceof NavigationEnd: {
+          const title = this.getTitle(this.router.routerState, this.router.routerState.root).join(' - ');
+          this.titleService.setTitle(`Exordium - ${title}`);
+
+          setTimeout(() => {
+            document.querySelector('body').classList.add('loaded');
+          }, 1000);
+        }
+        case event instanceof NavigationCancel: {}
+        case event instanceof NavigationError: {
+          setTimeout(() => {
+            document.querySelector('body').classList.add('loaded');
+          }, 1000);
+
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
   }
+
+  getTitle(state, parent) {
+    const data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+      data.push(parent.snapshot.data.title);
+    }
+
+    if (state && parent) {
+      data.push(... this.getTitle(state, state.firstChild(parent)));
+    }
+
+    return data;
+  }
+
 }
