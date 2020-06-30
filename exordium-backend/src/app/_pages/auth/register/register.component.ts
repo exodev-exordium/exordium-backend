02f8e504a8d5bb7da68bd81813850d5a534eaa09
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-29 23:55:03 -0400

    update; add registration page design

--- exordium-backend/src/app/_pages/auth/register/register.component.ts
+++ exordium-backend/src/app/_pages/auth/register/register.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { jarallax } from 'jarallax';
+
+import * as $ from 'jquery';
+
+import { AuthService } from 'src/app/__services/auth.service';
+import { UserService } from 'src/app/__services/user.service';
+
+import { Countries } from 'src/app/__services/vars/Countries';
+import { MustMatch } from 'src/app/__services/helpers/mustmatch.validator';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +18,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  countries: any;
+  selectedCountry: any;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { 
+    this.registerForm = this.formBuilder.group({
+      username: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      realname: [null],
+      country: [null],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+      confirmPassword: [null, Validators.required],
+      recaptcha: [null, Validators.required]
+    }, { validator: MustMatch('password', 'confirmPassword') });
+  }
 
   ngOnInit(): void {
+    jarallax(document.querySelectorAll('.jarallax'), {
+      speed: 0.6
+    });
+
+    this.countries = new Countries().countries;
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit(): void {
+    const srcButton = $('submitRegister');
+    srcButton.attr('disabled', true);
+    srcButton.addClass('m-progress');
+
+    if (this.registerForm.invalid) {
+      this.toastr.error('Please make sure you fill out all the fields correctly, then creating your account again...');
+    } else {
+      this.authService.signin(this.registerForm.value).subscribe((res) => {
+        this.toastr.success('Your account has been successfully created, you\'re free to login now!');
+
+        localStorage.setItem('access_token', res.token);
+
+        this.userService.getUserDataBasic().subscribe((res) => {
+          this.router.navigate([`/auth/signin`]);
+        });
+      }, (err) => {
+        this.toastr.success(err[0].msg);
+      });
+
+    }
+
+    setTimeout(() => {
+      srcButton.removeAttr('disabled');
+      srcButton.removeClass('m-progress');
+    }, 2000);
+
   }
 
 }
