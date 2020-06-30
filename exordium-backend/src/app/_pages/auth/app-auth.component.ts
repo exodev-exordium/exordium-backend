d93b87f5397a2c97b72a536519b73451d1788492
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-29 23:56:37 -0400

    update; main auth page designs

--- exordium-backend/src/app/_pages/auth/app-auth.component.ts
+++ exordium-backend/src/app/_pages/auth/app-auth.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-app-auth',
@@ -6,11 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-auth.component.scss']
 })
 export class AppAuthComponent implements OnInit {
+  windowScrolled: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
 
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    if (window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.windowScrolled = true;
+
+      if (!$('nav.navbar').hasClass('scrolled')) {
+        $('nav.navbar').addClass('position-fixed scrolled');
+      }
+
+      if (!$('.nav-dashboard-wrapper').hasClass('scrolled')) {
+        $('.nav-dashboard-wrapper').addClass('position-fixed scrolled');
+      }
+
+    } else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+      this.windowScrolled = true;
+
+      if ($('nav.navbar').hasClass('scrolled')) {
+        $('nav.navbar').removeClass('position-fixed scrolled');
+      }
+
+      if ($('.nav-dashboard-wrapper').hasClass('scrolled')) {
+        $('.nav-dashboard-wrapper').removeClass('position-fixed scrolled');
+      }
+    }
   }
 
 }
