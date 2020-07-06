20ef3434d20ad2d1ab1535a03961d02a74e8cc3e
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-06 05:50:53 -0400

    update; add ckeditor blog pages

--- exordium-backend/src/app/_pages/dashboard/management/blogs/staff-blog-add/staff-blog-add.component.ts
+++ exordium-backend/src/app/_pages/dashboard/management/blogs/staff-blog-add/staff-blog-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-staff-blog-add',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staff-blog-add.component.scss']
 })
 export class StaffBlogAddComponent implements OnInit {
+  public Editor = ClassicEditor;
 
   constructor() { }
 
