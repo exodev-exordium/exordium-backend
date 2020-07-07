bcfe816d2c1a682cadd773f91a1083b76b5b99fc
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-07 03:31:46 -0400

    update; contact request updates

--- exordium-backend/src/app/_pages/dashboard/management/support/staff-contact/staff-contact.component.ts
+++ exordium-backend/src/app/_pages/dashboard/management/support/staff-contact/staff-contact.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
+
+import { UserService } from 'src/app/__services/user.service';
+import { MContactService } from 'src/app/__services/management/m-contact.service';
 
 @Component({
   selector: 'app-staff-contact',
@@ -6,10 +12,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staff-contact.component.scss']
 })
 export class StaffContactComponent implements OnInit {
+  // User Autentication
+  currentUser: any;
 
-  constructor() { }
+  // Contact Emails Table
+  contactEmails: any[];
+  loadingIndicator = true;
+  selected = [];
+  columnMode = ColumnMode;
+  selectionType = SelectionType;
+
+  constructor(
+    private router: Router,
+    private modalService: NgbModal,
+    private userService: UserService,
+    private mContactService: MContactService
+  ) { }
 
   ngOnInit(): void {
+    this.userService.getUserDataBasic().subscribe(res => {
+      this.currentUser = res.response;
+
+      this.checkPermissions();
+    });
+  }
+
+  checkAccessPage(array, key, value) {
+    return array.some(object => object[key] === value);
+  }
+
+  checkPermissions() {
+    if (this.checkAccessPage(this.currentUser.access.pages, 'page', 'contact')) {
+
+      this.mContactService.contactEmails().subscribe(res => {
+        this.contactEmails = res;
+        console.log(this.contactEmails);
+        this.loadingIndicator = false;
+      });
+
+    } else {
+      this.router.navigate(['dashboard']);
+    }
+  }
+
+  onSelect({selected}) {
+    console.log(`Select Event: `, selected, this.selected);
+
+  }
+
+  onView(content) {
+    this.modalService.open(content, { centered: true, size: 'lg' });
   }
 
 }
