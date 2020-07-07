94ae180e047d43694bc71f22c5240190ca01973d
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-07 03:44:45 -0400

    update; users overview page

--- exordium-backend/src/app/_pages/dashboard/management/users/staff-users-overview/staff-users-overview.component.ts
+++ exordium-backend/src/app/_pages/dashboard/management/users/staff-users-overview/staff-users-overview.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
+
+import { UserService } from 'src/app/__services/user.service';
+import { MUsersService } from 'src/app/__services/management/m-users.service';
 
 @Component({
   selector: 'app-staff-users-overview',
@@ -6,10 +12,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staff-users-overview.component.scss']
 })
 export class StaffUsersOverviewComponent implements OnInit {
+  // User Autentication
+  currentUser: any;
 
-  constructor() { }
+  // Contact Emails Table
+  users: any[];
+  loadingIndicator = true;
+  selected = [];
+  columnMode = ColumnMode;
+  selectionType = SelectionType;
+
+  // Users specific
+  selectedRoles = [];
+  permissionAccess = [];
+
+  constructor(
+    private router: Router,
+    private modalService: NgbModal,
+    private userService: UserService,
+    private mUsersService: MUsersService
+  ) { }
 
   ngOnInit(): void {
+    this.userService.getUserDataBasic().subscribe(res => {
+      this.currentUser = res.response;
+
+      this.checkPermissions();
+    });
+  }
+
+  checkAccessPage(array, key, value) {
+    return array.some(object => object[key] === value);
+  }
+
+  checkPermissions() {
+    if (this.checkAccessPage(this.currentUser.access.pages, 'page', 'users-overview')) {
+
+      this.mUsersService.getUsers().subscribe(res => {
+        this.users = res;
+        console.log(this.users);
+        this.loadingIndicator = false;
+      });
+
+    } else {
+      this.router.navigate(['dashboard']);
+    }
+  }
+
+  onSelect({selected}) {
+    console.log(`Select Event: `, selected, this.selected);
+    this.selectedRoles = this.selected[0].access.roles.map((item) => {
+        return item.role
+    });
+    console.log('Roles: ', this.selectedRoles);
+  }
+
+  onView(content) {
+    this.modalService.open(content, { centered: true, size: 'lg' });
   }
 
 }
