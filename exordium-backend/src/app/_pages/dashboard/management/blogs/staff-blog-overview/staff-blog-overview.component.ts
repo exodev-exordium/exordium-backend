f9112e4cf77d6494eee90de5a96261264355219f
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-11 16:38:46 -0400

    update; blog management overview base design

--- exordium-backend/src/app/_pages/dashboard/management/blogs/staff-blog-overview/staff-blog-overview.component.ts
+++ exordium-backend/src/app/_pages/dashboard/management/blogs/staff-blog-overview/staff-blog-overview.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
+
+import { UserService } from 'src/app/__services/user.service';
+import { MBlogsService } from 'src/app/__services/management/m-blogs.service';
+
+import { Permissions } from 'src/app/__injectables/permissions.injectable';
+import { Roles } from 'src/app/__injectables/roles.injectable';
+
 
 @Component({
   selector: 'app-staff-blog-overview',
@@ -6,10 +15,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staff-blog-overview.component.scss']
 })
 export class StaffBlogOverviewComponent implements OnInit {
+  // User Autentication
+  currentUser: any;
 
-  constructor() { }
+  // Blog Posts Table
+  posts: any[];
+  loadingIndicator = true;
+  selected = [];
+  columnMode = ColumnMode;
+  selectionType = SelectionType;
+
+  constructor(
+    private modalService: NgbModal,
+    private userService: UserService,
+    private permissions: Permissions,
+    private mBlogService: MBlogsService,
+    public roles: Roles
+  ) { }
 
   ngOnInit(): void {
+    this.userService.getUserDataBasic().subscribe(res => {
+      this.currentUser = res.response;
+
+      if (this.permissions.checkPermissions(this.currentUser.access.pages, 'users-overview')) {
+        this.mBlogService.getBlogs().subscribe(res => {
+          this.posts = res;
+          this.loadingIndicator = false;
+        });
+      }
+    });
   }
+
+  onSelect({selected}) {
+    console.log(`Select Event: `, selected, this.selected);
+  }
+
+  onView(content) {
+    this.modalService.open(content, { centered: true, size: 'lg' });
+  }
+
 
 }
