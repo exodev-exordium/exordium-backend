b222ec8071dcdaa99cbc82677ba9fa58c8e93731
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-10 21:10:12 -0400

    update; design and functionality on add a new blog post management page

--- exordium-backend/src/app/_pages/dashboard/management/blogs/staff-blog-add/staff-blog-add.component.ts
+++ exordium-backend/src/app/_pages/dashboard/management/blogs/staff-blog-add/staff-blog-add.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+import { UserService } from 'src/app/__services/user.service';
+import { MBlogsService } from 'src/app/__services/management/m-blogs.service';
+import { Permissions } from 'src/app/__injectables/permissions.injectable';
+
+import * as $ from 'jquery';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 
 @Component({
   selector: 'app-staff-blog-add',
@@ -7,11 +16,65 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
   styleUrls: ['./staff-blog-add.component.scss']
 })
 export class StaffBlogAddComponent implements OnInit {
+  // User Autentication
+  currentUser: any;
+  loadingIndicator = true;
+
+  // Form Controls
+  addBlogForm: FormGroup;
   public Editor = ClassicEditor;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private mBlogService: MBlogsService,
+    private permissions: Permissions,
+    private toastr: ToastrService
+  ) { 
+    this.addBlogForm = this.formBuilder.group({
+      title: [null, [Validators.required]],
+      subtitle: [null],
+      url: [null, Validators.required],
+      bodylong: [null, Validators.required],
+      recaptcha: [null, Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+    this.userService.getUserDataBasic().subscribe(res => {
+      this.currentUser = res.response;
+
+      if (this.permissions.checkPermissions(this.currentUser.access.pages, 'blog-add')) {
+        this.loadingIndicator = false;
+      }
+    });
+  }
+
+  get f() {
+    return this.addBlogForm.controls;
+  }
+
+  onSubmit(): void {
+    const srcButton = $('.submitPost');
+    srcButton.attr('disabled', true);
+    srcButton.addClass('m-progress');
+
+    if (this.addBlogForm.invalid) {
+      this.toastr.error('Please make sure you fill out all the fields correctly, then try signing in again...');
+    } else {
+      this.mBlogService.addPost(this.addBlogForm.value).subscribe((res) => {
+        this.toastr.success(`Your blog has been successfully posted!`);
+      }, (err) => {
+        this.toastr.success(err[0].msg);
+      });
+
+    }
+
+    setTimeout(() => {
+      srcButton.removeAttr('disabled');
+      srcButton.removeClass('m-progress');
+    }, 2000);
+
   }
 
 }
