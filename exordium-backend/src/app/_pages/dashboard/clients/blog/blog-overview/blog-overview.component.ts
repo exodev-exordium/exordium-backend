0e8ca6b27bc845b6f7f6021d1cff48d397d07585
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-14 00:41:17 -0400

    initial; blog overview and blog article pages for clients

---
+++ exordium-backend/src/app/_pages/dashboard/clients/blog/blog-overview/blog-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-overview',
+  templateUrl: './blog-overview.component.html',
+  styleUrls: ['./blog-overview.component.scss']
+})
+export class BlogOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
